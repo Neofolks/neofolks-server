1ac371a9dc52c6862f243670516ed09cd0783308
OhmPatil <workpatil10@gmail.com>
2023-02-22 02:16:14 +0530

    Cleanup code

--- sender.js
+++ sender.js
@@ -1,26 +1,5 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-// const nodemailer = require("nodejs-nodemailer-outlook");
-
-// async function sender(email, name) {
-//   return nodemailer.sendEmail({
-//     auth: {
-//       user: process.env.USER,
-//       pass: process.env.PASS,
-//     },
-//     from: process.env.USER,
-//     to: email,
-//     subject: `Hey ${name}, you are awesome!`,
-//     html: "<b>Theres nothing in here</b>",
-//     text: "some dummy text content",
-//     replyTo: process.env.USER,
-
-//     onError: (e) => console.log(e),
-//     onSuccess: (i) => {
-//       console.log(`Mail sent to ${email}`);
-//     },
-//   });
-// }
 
 // Trying nodemailer instead of nodemailer-outlook
 // To prevent throttling for concurrent connections
